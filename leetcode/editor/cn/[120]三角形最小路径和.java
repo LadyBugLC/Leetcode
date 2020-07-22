bf340c3cb2574c4ad24526685cdc2206301f533f
LadyBugLC <896820997@qq.com>
2020-07-22 22:22:51 +0800

    leetcode

---
+++ leetcode/editor/cn/[120]三角形最小路径和.java
@@ -0,0 +1,46 @@
+//给定一个三角形，找出自顶向下的最小路径和。每一步只能移动到下一行中相邻的结点上。 
+//
+// 相邻的结点 在这里指的是 下标 与 上一层结点下标 相同或者等于 上一层结点下标 + 1 的两个结点。 
+//
+// 
+//
+// 例如，给定三角形： 
+//
+// [
+//     [2],
+//    [3,4],
+//   [6,5,7],
+//  [4,1,8,3]
+//]
+// 
+//
+// 自顶向下的最小路径和为 11（即，2 + 3 + 5 + 1 = 11）。 
+//
+// 
+//
+// 说明： 
+//
+// 如果你可以只使用 O(n) 的额外空间（n 为三角形的总行数）来解决这个问题，那么你的算法会很加分。 
+// Related Topics 数组 动态规划 
+// 👍 489 👎 0
+
+
+import java.util.List;
+
+//leetcode submit region begin(Prohibit modification and deletion)
+//动态规划,记录当前行角标位置处的最优值,从下向上
+class Solution {
+    public int minimumTotal(List<List<Integer>> triangle) {
+        int n = triangle.size();
+        int[] dp = new int[n];
+        for (int i = 0; i < n; i++)
+            dp[i] = triangle.get(n - 1).get(i);
+        for (int i = n - 2; i >= 0; i--) {
+            for (int j = 0; j <= i; j++) {
+                dp[j] = triangle.get(i).get(j) + Math.min(dp[j], dp[j + 1]);
+            }
+        }
+        return dp[0];
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
